2e77420629842428f218b9ca712b668a6f912153
Alexander Zheglov <zheglov@yandex-team.ru>
2017-02-08 14:34:03 +0300

    animation added. more jscriptification

--- js/color.js
+++ js/color.js
@@ -1,4 +1,6 @@
-COLORS=['red', 'green', 'blue'];
+var COLORS=['red', 'green', 'blue'];
+var current_recipe = undefined;
+var current_title = undefined;
 
 function tohex(x) {
     x = parseInt(x);
@@ -12,56 +14,58 @@ function tohex(x) {
     return ret;
 }
 
-function tocolor(r, g, b) {
-    return '#' + tohex(r) + tohex(g) + tohex(b)
+function tocolor(rgb) {
+    return '#' + rgb.map(c => { return tohex(c); }).join('');
 }
 
 function to_init_color(color, value) {
     switch(color) {
         case 'red':
-            return tocolor(value, 0, 0);
+            return tocolor([value, 0, 0]);
         case 'green':
-            return tocolor(0, value, 0);
+            return tocolor([0, value, 0]);
         case 'blue':
-            return tocolor(0, 0, value)
+            return tocolor([0, 0, value])
         default:
             return undefined;
     }
 }
 
-function value_to_range(v) {
-    if (v<10)
-        ret = 0;
-    else
-        ret = Math.floor((v-10)/25);
-
-    return ret;
-}
+function value_to_range(v) { return v < 10 ? 0 : Math.floor((v-10)/25); }
+function range_to_value(r) { return 25*r + 10; }
 
 function sel_clicked() {
     // switch border
-    current_bordered = $(this).parent().find(".bordered")
-    current_bordered.removeClass('bordered')
-    $(this).addClass('bordered')
+    $(this).parent().find(".bordered").removeClass('bordered');
+    $(this).addClass('bordered');
 
-    // recolor circle
+    // get wish values
     var wish = COLORS.map(function (color) {
-        var r_selected = $('#' + color + '-selector').find(".bordered")[0];
-        var v = r_selected.id.replace(color, '');
-        return 25*v + 10;
-        })
-    console.log(wish);
+        return $('#' + color + '-selector').find(".bordered")[0].id.replace(color, '');
+    })
 
-    wcolor = tocolor(wish[0], wish[1], wish[2]);
-    $('#cc').css('background-color', wcolor);
-//    $('#help').css('background-color', wcolor);
-    $('#cc').prop('title', wcolor);
+    // recolor
+    wcolor = tocolor(wish.map(c => { return range_to_value(c);}));
+    $('#cc').css('background-color', wcolor)
+            .prop('title', wcolor);
+
+    // if current recipe hit - display color title
+    console.log(wish + ' ' + current_recipe + ' ' + current_title);
+    if ( current_recipe &&
+         wish[0] == current_recipe[0] &&
+         wish[1] == current_recipe[1] &&
+         wish[2] == current_recipe[2]) {
+
+        $('.colorname').text(current_title);
+        $('.colorname').fadeIn(1000);
+
+    }
+        
 }
 
 function create_selectors() {
-    for (c=0; c<COLORS.length; c++) {
-        color = COLORS[c];
-        div_html = ''
+    COLORS.forEach( color => {
+        var div_html = ''
 
         for (i=0; i<10; i++) {
             div_html += '<div class="selelem" id="' + color + i +'"></div>'
@@ -70,31 +74,30 @@ function create_selectors() {
         $('#' + color + '-selector').html(div_html)
 
         for (i=0; i<10; i++) {
-            $('#'+ color +i).css('background-color', to_init_color(color, 25*i + 10));
+            $('#'+ color +i).css('background-color', to_init_color(color, range_to_value(i)));
         }
 
         $('#' + color +'0').addClass('bordered');
-    }
+    })
 }
 
-function get_sample_by_id(id) {
-    return SAMPLES.find( item => {return item.id == id} )
+function get_sample_by_id(id) { return SAMPLES.find( item => {return item.id == id} ) }
+
+function set_sample_data(sample_id, sample) {
+    $(sample_id).css('background-color', sample.color)
+                .prop('title', sample.id)
+                .data('cid', sample.id);
 }
 
 function create_samples() {
     for (i=0; i<CURR_SAMPLES.length; i++) {
         sample = get_sample_by_id(CURR_SAMPLES[i]);
-        sample_id = '#sample'+i;
-        $(sample_id).css('background-color', sample.color)
-                    .prop('title', sample.id)
-                    .data('cid', sample.id);
+        set_sample_data('#sample'+i, sample);
     }
 }
 
-
-
 function create_recipe(o) {
-    recipe = '<div class="recipe_circle" style="background-color:#cc0000; background:radial-gradient(#ff0000, #7f0000)"><div class="recipe_symbol">' + value_to_range(o.colors[0]) + '</div></div>+'
+    var recipe = '<div class="recipe_circle" style="background-color:#cc0000; background:radial-gradient(#ff0000, #7f0000)"><div class="recipe_symbol">' + value_to_range(o.colors[0]) + '</div></div>+'
     recipe += '<div class="recipe_circle" style="background-color:#00cc00; background:radial-gradient(#00ff00, #007f00)"><div class="recipe_symbol">' + value_to_range(o.colors[1]) + '</div></div>+'
     recipe += '<div class="recipe_circle" style="background-color:#0000cc; background:radial-gradient(#0000ff, #00007f)"><div class="recipe_symbol">' + value_to_range(o.colors[2]) + '</div></div>='
     recipe += '<div class="recipe_circle" style="background-color:' + o.color + '"><div class="recipe_symbol" style="color:' + o.color + '">:)</div></div>'
@@ -103,13 +106,13 @@ function create_recipe(o) {
 }
 
 function get_random_sample() {
-    index = Math.floor(Math.random() * SAMPLES.length)
+    var index = Math.floor(Math.random() * SAMPLES.length)
     return SAMPLES[index];
 }
 
 function redraw_sample(who) {
     for (;;) {
-        sample = get_random_sample()
+        var sample = get_random_sample()
         if (CURR_SAMPLES.indexOf(sample.id) == -1)
             break
     }
@@ -117,18 +120,17 @@ function redraw_sample(who) {
     CURR_SAMPLES.splice(CURR_SAMPLES.indexOf(who.title), 1);
     CURR_SAMPLES.push(sample.id);
 
-    who.css('background-color', sample.color)
-        .prop('title', sample.id)
-        .data('cid', sample.id);
+    set_sample_data(who, sample);
 }
 
 function sample_clicked() {
-    var help_text = ''
-
-    so = get_sample_by_id($(this).data().cid)
+    var so = get_sample_by_id($(this).data().cid)
     $('.help').show();
+    $('.colorname').fadeOut(1400);
 
     if (so) {
+        current_recipe = so.colors.map(c => { return value_to_range(c) });
+        current_title = so.id;
         $('#help').html(create_recipe(so));
         var cw = $('.recipe_circle').width();
         $('.recipe_circle').css({'height':cw+'px'});
@@ -144,5 +146,6 @@ $(document).ready(function() {
 
     $('.selelem').click(sel_clicked);
     $('.sample_circle').click(sample_clicked);
-    $('.help').click(function() { $(this).hide(); });
+    $('.help').click(function() { $(this).hide(); $('.colorname').fadeOut(1400); });
+    $('.colorname').hide();
 })
